fd7ae47f622d5500cd06f28909147c2ec2b3252e
Nathaniel Mott <mottnathaniel@gmail.com>
2023-07-23 19:42:31 -0400

    basic functionality added

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use chrono::prelude::*;
 use clap::Parser;
 use eyre::ErrReport;
-use std::{fs, io};
+use std::fs;
 
 #[derive(Parser)]
 #[command(name = "scry")]
 #[command(author = "Nathaniel Mott")]
 #[command(version = "0.1.0")]
 struct Cli {
+    ///The file we want to retrieve information about
     file: Option<String>,
-    // command: Option<Commands>,
+    ///Gather metadata about the specified file
+    #[arg(short, long)]
+    metadata: bool,
+    ///Analyze the specified file's contents
+    #[arg(short, long)]
+    content: bool,
 }
 
 // #[derive(Subcommand)]
@@ -23,6 +29,13 @@ struct FileData {
     created: String,
     modified: String,
     accessed: String,
+}
+
+#[derive(Debug)]
+struct FileAnalysis {
+    word_count: usize,
+    line_count: usize,
+    paragraph_count: usize,
 }
 
 fn get_data(file: &str) -> eyre::Result<FileData, ErrReport> {
@@ -44,9 +57,40 @@ fn get_data(file: &str) -> eyre::Result<FileData, ErrReport> {
     })
 }
 
-fn main() {
+fn analyze_file(file: &str) -> eyre::Result<FileAnalysis, ErrReport> {
+    let contents: String = fs::read_to_string(file)?;
+    let whitespaced_words: Vec<&str> = contents.split(" ").collect();
+    let word_count = whitespaced_words.len();
+    let split_lines: Vec<&str> = contents.split("\n").collect();
+    let line_count = split_lines.len();
+    let split_grafs: Vec<&str> = contents.split("\n\n").collect();
+    let paragraph_count = split_grafs.len();
+
+    Ok(FileAnalysis {
+        word_count,
+        line_count,
+        paragraph_count,
+    })
+}
+
+fn main() -> eyre::Result<(), ErrReport> {
     let cli = Cli::parse();
-    let file = cli.file.as_deref().expect("Couldn't find file.");
-    let data = get_data(file);
-    println!("{:?}", data);
+    let file = cli.file.as_deref().unwrap();
+
+    if cli.metadata == true {
+        let data = get_data(file)?;
+        println!(
+            "Size:     {} bytes\nCreated:  {}\nModified: {}\nAccessed: {}\n",
+            data.size, data.created, data.modified, data.accessed
+        );
+    }
+
+    if cli.content == true {
+        let analysis = analyze_file(file)?;
+        println!(
+            "Words:      {}\nLines:      {}\nParagraphs: {}\n",
+            analysis.word_count, analysis.line_count, analysis.paragraph_count
+        );
+    }
+    Ok(())
 }
